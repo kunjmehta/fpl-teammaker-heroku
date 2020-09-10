612abeb53c1339a0ad2465f940ae3dc2d0d71023
kunjmehta <32904245+kunjmehta@users.noreply.github.com>
2020-09-10 23:49:54 +0530

    Fixed errors

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-mkdir -p ~/.streamlit/echo “\
+mkdir -p ~/.streamlit/
+echo “\
 [general]\n\
 email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.tomlecho “\
+“ > ~/.streamlit/credentials.toml
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
